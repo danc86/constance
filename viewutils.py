e6dc33b4701f4d8d5e19bf3e8c4b42da2a7f6be1
Dan Callaghan <djc@djc.id.au>
2008-06-08 01:10:54 +1000

    proper comma-separated lists for categories and tags
    committer: Dan Callaghan <djc@djc.id.au>
    
    --HG--
    extra : convert_revision : be9f0ece3d64b81e9db9f513e91e617c6ffd7e09

--- viewutils.py
+++ viewutils.py
@@ -2,9 +2,20 @@ import re
 import markdown
 import genshi
 
+from app import BASE_URL
+
 def mini_markdown(s):
 	# XXX find a more efficient way to do this?
 	m = markdown.Markdown(extensions=['typography']).convert(s)
 	the_p, = re.match(u'<p>(.*)\n</p>', m).groups()	
 	return genshi.Markup(the_p)
 
+def category_list(categories):
+	return genshi.Markup(u', ').join(
+			genshi.Markup(u'<a href="%s/+categories/%s">%s</a>' % (BASE_URL, category, category)) 
+			for category in categories)
+
+def tag_list(tags):
+	return genshi.Markup(u', ').join(
+			genshi.Markup(u'<a rel="tag" href="%s/+tags/%s">%s</a>' % (BASE_URL, tag, tag)) 
+			for tag in tags)
