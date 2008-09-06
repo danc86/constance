0aeb704c976e6724f022261ceb5e557949ca816f
Dan Callaghan <djc@djc.id.au>
2008-09-06 13:47:36 +1000

    reading log at /+reading/

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ class Constance(RegexApplication):
     urls = [(r'^$', 'index'), 
             (r'^feed$', 'feed'), 
             (r'^\+tags/(.+)$', 'tag'), 
+            (r'^\+reading/?$', 'reading'), 
             (r'^([^+/][^/]*)/?$', 'post')]
     charset = 'utf-8'
 
@@ -56,7 +57,7 @@ class Constance(RegexApplication):
                     environ=self.request.environ, 
                     title=None, 
                     self_url='%s/' % self.request.environ['APP_URI'], 
-                    sorted_entries=sorted_entries[:self.config.getint('global', 'entries_per_page')], 
+                    sorted_entries=sorted_entries[:self.config.getint('global', 'entries_in_feed')], 
                     feed_updated=sorted_entries[0].modified_date
                     ).render('xml')
             return HttpResponse(rendered, [('Content-Type', 'application/atom+xml')], 200)
@@ -99,7 +100,31 @@ class Constance(RegexApplication):
                     environ=self.request.environ, 
                     title=u'“%s” tag' % tag, 
                     self_url='%s/+tags/%s' % (self.request.environ['APP_URI'], tag.encode(self.charset)), 
-                    sorted_entries=sorted_entries[:self.config.getint('global', 'entries_per_page')], 
+                    sorted_entries=sorted_entries[:self.config.getint('global', 'entries_in_feed')], 
+                    feed_updated=sorted_entries[0].modified_date
+                    ).render('xml')
+            return HttpResponse(rendered, [('Content-Type', 'application/atom+xml')], 200)
+        else:
+            raise PageNotFound('Unknown format %r' % format)
+
+    def reading(self):
+        sorted_entries = sorted(self.readinglog_entries, key=lambda e: e.publication_date, reverse=True)
+        format = self.request.args.get('format', 'html')
+        if format == 'html':
+            rendered = template_loader.load('multiple.xml').generate(
+                    config=self.config, 
+                    environ=self.request.environ, 
+                    title=u'reading log', 
+                    sorted_entries=sorted_entries, 
+                    ).render('xhtml')
+            return HttpResponse(rendered, [('Content-Type', 'text/html')], 200)
+        elif format == 'atom':
+            rendered = template_loader.load('multiple_atom.xml').generate(
+                    config=self.config, 
+                    environ=self.request.environ, 
+                    title=u'reading log', 
+                    self_url='%s/+reading/' % self.request.environ['APP_URI'], 
+                    sorted_entries=sorted_entries[:self.config.getint('global', 'entries_in_feed')], 
                     feed_updated=sorted_entries[0].modified_date
                     ).render('xml')
             return HttpResponse(rendered, [('Content-Type', 'application/atom+xml')], 200)
